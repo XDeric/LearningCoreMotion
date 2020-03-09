bb6c5fe33c89f50722249eb5958763854f11bbaf
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-03-09 16:50:00 -0400

    added animations for instructions

--- CoreMotionTest/MovingImage.swift
+++ CoreMotionTest/MovingImage.swift
@@ -16,15 +16,50 @@ class MovingImage: UIViewController {
     let motionManager = CMMotionManager()
     var scene = GameScene()
     
+    lazy var instructions: UILabel = {
+        let text = UILabel()
+        text.text = "Tilt and Find Me!"
+        text.numberOfLines = 3
+        text.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        text.alpha = 0
+        return text
+    }()
+    
     lazy var button: UIButton = {
+        let button = UIButton()
+        button.sizeToFit()
+        button.isHidden = true
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.6798086851, green: 0.9229053351, blue: 0.9803921569, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setTitle("You Found Me", for: .normal)
+        button.addTarget(self, action: #selector (nextVC), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var falseButton: UIButton = {
         let button = UIButton()
         button.sizeToFit()
         button.isHidden = false
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = #colorLiteral(red: 0.6798086851, green: 0.9229053351, blue: 0.9803921569, alpha: 1)
         button.layer.cornerRadius = 5
-        button.setTitle("You Found Me", for: .normal)
-        button.addTarget(self, action: #selector(nextVC), for: .touchUpInside)
+        button.setTitle("Try Me", for: .normal)
+        button.addTarget(self, action: #selector (nextVC), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var fakeButton: UIButton = {
+        let button = UIButton()
+        button.sizeToFit()
+        button.isHidden = false
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.6798086851, green: 0.9229053351, blue: 0.9803921569, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setTitle("Ka-Boom", for: .normal)
+        button.addTarget(self, action: #selector (nextVC), for: .touchUpInside)
         
         return button
     }()
@@ -36,28 +71,52 @@ class MovingImage: UIViewController {
     }
     
     func setupConstraints(){
+        instructions.translatesAutoresizingMaskIntoConstraints = false
         button.translatesAutoresizingMaskIntoConstraints = false
+        falseButton.translatesAutoresizingMaskIntoConstraints = false
+        fakeButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(instructions)
         view.addSubview(button)
+        view.addSubview(falseButton)
+        view.addSubview(fakeButton)
         
         NSLayoutConstraint.activate([
             button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             button.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            
+            instructions.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            instructions.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            falseButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            fakeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
     
     func startingAnimation(){
-        UIImageView.animateKeyframes(withDuration: 2, delay: 0, options: .beginFromCurrentState, animations:  {
-            
+        UIView.animateKeyframes(withDuration: 5, delay: 0, options: [.calculationModePaced], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 2.5, animations: {
+                self.instructions.alpha = 1
+                self.instructions.transform = CGAffineTransform(scaleX: 2, y: 2)
+            })
+            UIView.addKeyframe(withRelativeStartTime: 2.5, relativeDuration: 2.5, animations: {
+                self.instructions.alpha = 0
+                //                self.instructions.transform = CGAffineTransform(scaleX: 0, y: 0)
+            })
         }) { (Bool) in
-        
+            //            self.presentScene()
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
         view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         presentScene()
         setupConstraints()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startingAnimation()
     }
     
     //MARK: - Private Funcs
---
+++ CoreMotionTest/extension.swift
@@ -0,0 +1,36 @@
+//
+//  extension.swift
+//  CoreMotionTest
+//
+//  Created by EricM on 3/9/20.
+//  Copyright © 2020 EricM. All rights reserved.
+//
+
+import UIKit
+//
+//extension UIViewController {
+//    
+//    typealias buttonFunction = () -> Void
+//    
+//    func makeButton(name: String, buttonText: String, function: buttonFunction)-> UIButton{
+//        let name = UIButton()
+//        name.sizeToFit()
+//        name.isHidden = false
+//        name.setTitleColor(.black, for: .normal)
+//        name.backgroundColor = #colorLiteral(red: 0.6798086851, green: 0.9229053351, blue: 0.9803921569, alpha: 1)
+//        name.setTitle(buttonText, for: .normal)
+//        name.layer.cornerRadius = 5
+//        name.addTarget(self, action: #selector(function), for: .touchUpInside)
+//        
+//        return name
+//    }
+//}
+//
+//extension UIViewController {
+//    
+//    @objc func nextVC(viewController: UIViewController){
+//        let vc = viewController
+//        vc.modalPresentationStyle = .fullScreen
+//        present(vc, animated: true, completion: nil)
+//    }
+//}
